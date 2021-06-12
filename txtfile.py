308c1f8e87e3497f884d524fa92b1515bd099b68
sapnaxz <64073632+sapnaxz@users.noreply.github.com>
2021-06-12 13:49:14 +0530

    Add files via upload

---
+++ txtfile.py
@@ -0,0 +1,11 @@
+#count the number of words present in the text file
+fname = input("Enter file name: ")
+ 
+num_words = 0
+ 
+with open(fname, 'r') as f:
+    for line in f:
+        words = line.split()
+        num_words += len(words)
+print("Number of words:")
+print(num_words)
