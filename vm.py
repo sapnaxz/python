9af21e82bbf0c3592b14161a8bd9ec7f662f7c0e
sapnaxz <64073632+sapnaxz@users.noreply.github.com>
2021-08-23 17:31:02 +0530

    Add files via upload
    AI reciting a few lines of lose yourself to the music by eminem.

---
+++ vm.py
@@ -0,0 +1,9 @@
+import pyttsx3
+import pythoncom
+import pywintypes
+
+engine = pyttsx3.init()
+engine.say("His palms are sweaty, knees weak, arms are heavyThere's vomit on his sweater already, mom's spaghettiHe's nervous, but on the surface he looks calm and ready To drop bombs, but he keeps on forgettin' What he wrote down, the whole crowd goes so loud He opens his mouth, but the words won't come out He's chokin', how, everybody's jokin' now The clocks run out, times up, over, blaow Snap back to reality, ope there goes gravity")
+engine.runAndWait()
+
+#
